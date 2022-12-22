d0ca1212de13d81f7623384bd4c49b86ee6c26fd
imbharat420 <imbharat420@gmail.com>
2022-12-23 02:17:08 +0530

    steal typescript

---
+++ react/hook/useMount.ts
@@ -0,0 +1,28 @@
+import { useEffect,useRef } from 'react';
+
+
+const useMount = (effect: () => void) => useEffect(effect, []);
+
+
+const useMountRef = () => {
+  const mountRef = useRef(true);
+  useMount(() => () => mountRef.current = false);
+  return mountRef;
+};
+
+
+const useMountedRef = () => {
+  const ref = useRef(true);
+
+  useMount(() => () => ref.current = false);
+  return ref;
+};
+
+const useUnMount = (effect: any, deps: any[] = []) => {
+  const mountedRef = useMountRef();
+
+  useEffect(() => () => {
+    !mountedRef.current && effect();
+  }, deps);
+};
+
